2a65cfc3640bb4bfc396d03cd2d722f88d7625b5
AlejandrodePablo <115726867+AlejandrodePablo@users.noreply.github.com>
2024-02-08 19:55:24 +0100

    Initial commit

---
+++ src/main/java/Alejandro/Alonso/DAM/EjercicioCallOfDuty/App.java
@@ -0,0 +1,13 @@
+package Alejandro.Alonso.DAM.EjercicioCallOfDuty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
