d084429b114fe671425246c366d93041da1dacd8
Borja <borjajg02@gmail.com>
2024-02-28 16:51:00 +0100

    Equipamiento
    Equipamiento

--- src/main/java/Alejandro/Alonso/DAM/EjercicioCallOfDuty/Equipamiento.java
+++ src/main/java/Alejandro/Alonso/DAM/EjercicioCallOfDuty/Equipamiento.java
@@ -1,5 +1,33 @@
 package Alejandro.Alonso.DAM.EjercicioCallOfDuty;
 
 public class Equipamiento {
+	
+	 String nombre;
+	    int peso;
+
+	 public Equipamiento(String nombre, int peso) {
+		 this.nombre = nombre;
+	     this.peso = peso;
+	 }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	  
+	  
+	
+	
     
 }
